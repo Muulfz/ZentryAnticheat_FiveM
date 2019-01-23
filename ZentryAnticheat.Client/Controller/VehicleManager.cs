8892da9f637ec3868ac123afb26cb12f05639970
Muulfz <mattreisro@gmail.com>
2019-01-23 00:54:45 -0200

    Adicionado Vehicle!

--- ZentryAnticheat.Client/Controller/VehicleManager.cs
+++ ZentryAnticheat.Client/Controller/VehicleManager.cs
@@ -1,13 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using CitizenFX.Core;
+using CitizenFX.Core.Native;
 
 namespace ZentryAnticheat.Client.Controller
 {
     public class VehicleManager
     {
-
+            
+        private int[] vehicleIdInterzise = new int[1]
+        {
+            21321321
+        };
+        
         public void VehicleScan()
         {
-            Debug.WriteLine("TESTE ");
+            foreach (Vehicle allVehicle in World.GetAllVehicles())
+            {
+                int entityModel = API.GetEntityModel(allVehicle.Handle);
+                if (API.DoesEntityExist(allVehicle.Handle))
+                {
+                    if (((IEnumerable<int>) this.vehicleIdInterzise).Contains<int>(entityModel))
+                    {
+                        allVehicle.Delete();
+                    }
+                }
+            }
             
         }
     }
